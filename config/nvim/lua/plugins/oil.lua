a70e0f2f020252a0e97bd6f87257f2bc6af70c8e
Willian Fernandes <eu@willianfernandes.com.br>
2024-06-23 11:36:40 -0300

    Skip Oil confirmation popup for simple edits

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,9 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  opts = {
+    skip_confirm_for_simple_edits = true,
+  },
   init = function()
     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
   end,
