253fb3e8b6d8fc5bc5f3da54d239a257e7237279
Willian Fernandes <eu@willianfernandes.com.br>
2024-07-25 10:33:04 -0300

    Add extras options based on their documentations

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Prettier formatter options
+-- For reference: https://www.lazyvim.org/extras/formatting/prettier#options
+vim.g.lazyvim_prettier_needs_config = false
+
+-- Python lang options
+-- For reference: https://www.lazyvim.org/extras/lang/python#options
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
+
+-- Ruby lang options
+-- For reference: https://www.lazyvim.org/extras/lang/ruby#options
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
