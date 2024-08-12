8acaffcb7f8e9e5ba195873990a1be996025b36c
Willian Fernandes <eu@willianfernandes.com.br>
2024-08-12 11:08:01 -0300

    [NeoVim] Update LSPConfig to use ruby-lsp from the system path instead of from Mason's

---
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,12 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruby_lsp = {
+        mason = false,
+        enabled = true,
+        cmd = { os.getenv("HOME") .. "/.asdf/shims/ruby-lsp" },
+      },
+    },
+  },
+}
