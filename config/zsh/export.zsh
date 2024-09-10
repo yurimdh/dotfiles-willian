02e5aaf8b9d21130962044ce0bb675413cb94a90
Willian Fernandes <eu@willianfernandes.com.br>
2024-09-10 20:51:15 -0300

    ZSH: Use Catppuccin theme for FZF

--- config/zsh/export.zsh
+++ config/zsh/export.zsh
@@ -13,3 +13,20 @@ export COMPOSE_HTTP_TIMEOUT=120
 export RUST_WITHOUT=rust-docs
 
 export RUBY_YJIT_ENABLE=1
+
+# Use Catppuccin theme for FZF
+if [[ $TERM_IS_DARK == "true" ]]; then
+  export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
+else
+  export FZF_DEFAULT_OPTS=" \
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+--color=selected-bg:#bcc0cc \
+--multi"
+fi
