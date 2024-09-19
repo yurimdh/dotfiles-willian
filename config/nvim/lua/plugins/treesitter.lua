ca55fd937c80043277bb55e586dd98c295430dbb
Willian Fernandes <eu@willianfernandes.com.br>
2024-09-19 14:28:54 -0300

    Neovim: Configure nvim-treesitter-endwise

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "RRethy/nvim-treesitter-endwise",
+  },
   opts = {
     auto_install = true,
     indent = {
       enable = true,
       disable = { "ruby" },
+    },
+    endwise = {
+      enable = true,
     },
   },
 }
