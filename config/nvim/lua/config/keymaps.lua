6b5daa1433e54fc0e0776712f084e0b59333d634
Willian Fernandes <eu@willianfernandes.com.br>
2024-11-08 17:54:49 -0300

    Neovim: fix keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-
-  -- do not create the keymap if a lazy keys handler exists
-  ---@diagnostic disable-next-line: undefined-field
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+local map = vim.keymap.set
 
 -- Normal
 -- Increment/decrement
@@ -52,8 +39,9 @@ map("n", "<C-l>", ":<C-U>TmuxNavigateRight<CR>", { silent = true })
 map("n", "<C-\\>", ":<C-U>TmuxNavigatePrevious<CR>", { silent = true })
 
 -- Easier way to delete current buffer
-map("n", "<S-q>", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
-
+map("n", "<S-q>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 -- Visual --
 
 -- move selected lines
